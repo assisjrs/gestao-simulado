37a380227d1658aa1b901237da939d5a8c5e9285
assisjrs <assisjrs@gmail.com>
2019-10-25 10:00:26 -0300

    initial commit

---
+++ src/main/java/work/assisjrs/simulado/simuladoapi/SimuladoApiApplication.java
@@ -0,0 +1,13 @@
+package work.assisjrs.simulado.simuladoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/assisjrs/simulado/simuladoapi/SimuladoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package work.assisjrs.simulado.simuladoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
