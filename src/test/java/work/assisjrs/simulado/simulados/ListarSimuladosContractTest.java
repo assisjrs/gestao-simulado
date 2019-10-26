f456de540b376ebd0b823088f698b9a66f11985d
assisjrs <assisjrs@gmail.com>
2019-10-26 20:35:00 -0300

    mais testes de contrato

--- src/test/java/work/assisjrs/simulado/simulados/ListarSimuladosContractTest.java
+++ src/test/java/work/assisjrs/simulado/simulados/ListarSimuladosContractTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import static com.jayway.restassured.RestAssured.get;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasKey;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.OK;
 import static work.assisjrs.simulado.Helper.url;
@@ -31,5 +32,19 @@ public class ListarSimuladosContractTest {
                 .thenReturn().jsonPath().get();
 
         assertThat(itens).isNotEmpty();
+    }
+
+    @Test
+    public void deve_existir_o_campo_id() {
+        get(url(port,"/simulados"))
+       .then()
+       .body("[0]", hasKey("id"));
+    }
+
+    @Test
+    public void deve_existir_o_campo_nome() {
+        get(url(port,"/simulados"))
+                .then()
+                .body("[0]", hasKey("nome"));
     }
 }
