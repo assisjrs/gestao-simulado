6d529cbc1e1084acca75b2ee9f31790e7fb8b8cc
assisjrs <assisjrs@gmail.com>
2019-10-30 19:28:28 -0300

    criada a capacidade de recuperar uma prova por referencia do simulado e referencia da prova

--- src/main/java/work/assisjrs/simulado/questoes/QuestaoResource.java
+++ src/main/java/work/assisjrs/simulado/questoes/QuestaoResource.java
@@ -1,5 +1,6 @@
 package work.assisjrs.simulado.questoes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,13 @@ import static work.assisjrs.simulado.questoes.Nivel.FACIL;
 @RestController
 @RequestMapping("/simulados/{referenciaSimulado}/provas/{referenciaProva}")
 public class QuestaoResource {
+
+    @Autowired
+    private QuestaoService service;
+
     @GetMapping
     public ResponseEntity<?> get(@PathVariable final String referenciaSimulado, @PathVariable final String referenciaProva) {
-        final List<ProvaComQuestoesResponse> provasComQuestoes = new ArrayList<ProvaComQuestoesResponse>();
+        final List<ProvaComQuestoesResponse> provasComQuestoes = new ArrayList<>();
 
         final ProvaComQuestoesResponse provaComQuestoesResponse = new ProvaComQuestoesResponse();
         provaComQuestoesResponse.setQuestoes(new ArrayList<>());
